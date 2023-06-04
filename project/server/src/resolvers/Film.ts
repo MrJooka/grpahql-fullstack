55d28c83dfa3c5304e95f04d8720eacedbb9dbdd
MrJooka <mr.jooka@gmail.com>
2023-06-04 18:18:01 +0900

    type-graphql 활용 스키마 설계

---
+++ project/server/src/resolvers/Film.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from 'type-graphql';
+import ghibliData from '../data/ghibli';
+import { Film } from '../entities/Film';
+
+@Resolver(Film)
+export class FilmResolver {
+  @Query(() => [Film])
+  films(): Film[] {
+    return ghibliData.films;
+  }
+}
+
+type Query = {
+  films: Film[];
+};
