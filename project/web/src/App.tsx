107d19f3a0d221322f3132a9e2d70bfc5853eae3
MrJooka <mr.jooka@gmail.com>
2023-06-04 22:24:55 +0900

    FilmList - 영화 데이터 useQuery

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -5,13 +5,24 @@ import {
   Text,
   theme,
 } from "@chakra-ui/react"
+import FilmList from "./components/film/FilmList"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+})
 
 export const App = () => (
+  <ApolloProvider client={apolloClient}>
+
   <ChakraProvider theme={theme}>
     <Box>
       <Text>
         Ghibli GraphQl
+    <FilmList />
       </Text>
     </Box>
   </ChakraProvider>
+  </ApolloProvider>
 )
---
+++ project/web/src/components/film/FilmList.tsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+
+
+const FILM_QUERY = gql`
+  query ExampleQuery {
+    films {
+      id
+      title
+      subtitle
+    }
+  } 
+`
+
+export default function FilmList() {
+  const {data, loading, error} = useQuery(FILM_QUERY);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {JSON.stringify(error)}</p>
+
+
+  return (
+    <pre>
+     {
+        JSON.stringify(data, null, 2)
+     }
+    </pre>
+  )
+}
+
